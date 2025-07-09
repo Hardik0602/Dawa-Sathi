c1153c153fcf319d16030c60fc35ddaeb6f9200e
Hardik <pathakhardik2004@gmail.com>
2025-07-09 15:00:27 +0530

    Initial force push

---
+++ App.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { View } from 'react-native';
+import Register from './src/pages/register/Register';
+import Login from './src/pages/login/Login';
+import Welcome from './src/pages/welcome/Welcome';
+import Forgetpass from './src/pages/forgetpass/Forgetpass';
+import OTP from './src/pages/otp/OTP';
+import Name from './src/pages/name/Name';
+import Email from './src/pages/email/Email';
+import Gender from './src/pages/gender/Gender';
+import Location from './src/pages/location/Location';
+import Age from './src/pages/age/Age';
+import StartUp from './src/pages/startup/Startup';
+import AllRoutes from './src/navigation/AllRoutes';
+import MainTab from "./src/navigation/MainTab";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// const App = () => {
+//   return (
+//     <View className="h-full">
+//       <AllRoutes />
+//       {/* <Register /> */}
+//       {/* <Login /> */}
+//       {/* <Welcome /> */}
+//       {/* <Forgetpass /> */}
+//       {/* <OTP /> */}
+//       {/* <Name /> */}
+//       {/* <Email /> */}
+//       {/* <Gender /> */}
+//       {/* <Location /> */}
+//       {/* <Age /> */}
+//       {/* <StartUp/> */}
+//     </View>
+//   )
+// }
+
+// const App = () => {
+//   const [showStartup, setShowStartup] = useState(true);
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setShowStartup(false);
+//     }, 3000);
+//     return () => clearTimeout(timer);
+//   }, []);
+//   return showStartup ? <StartUp /> : <AllRoutes />;
+// }
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='StartUp' screenOptions={{ headerShown: false, animation: 'fade_from_bottom' }}>
+        <Stack.Screen name='StartUp' component={StartUp} />
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Forgetpass' component={Forgetpass} />
+        <Stack.Screen name='Register' component={Register} />
+        <Stack.Screen name='Name' component={Name} />
+        <Stack.Screen name='Email' component={Email} />
+        <Stack.Screen name='Welcome' component={Welcome} />
+        <Stack.Screen name='OTP' component={OTP} />
+        <Stack.Screen name='Gender' component={Gender} />
+        <Stack.Screen name='Location' component={Location} />
+        <Stack.Screen name='Age' component={Age} />
+        <Stack.Screen name='MainTab' component={MainTab} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
---
+++ src/pages/startup/Startup.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { View, Image } from 'react-native';
+import logo from '../../../images/logo.png';
+const Startup = ({ navigation }) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigation.replace('Welcome');
+        }, 1000); // 1 second
+        return () => clearTimeout(timer);
+    }, []);
+    return (
+        <View className=" flex-1 justify-center items-center bg-[#F5F6FA]">
+            <Image
+                source={logo}
+                resizeMode='contain'
+                className='h-full w-[80%]' />
+        </View>
+    );
+};
+export default Startup;
