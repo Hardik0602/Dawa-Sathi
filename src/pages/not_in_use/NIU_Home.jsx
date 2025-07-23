b83b8e885c009cdbf62c695adcbedbaaadaf3e47
Hardik <pathakhardik2004@gmail.com>
2025-07-23 19:02:21 +0530

    api, keyboard, safe area view left

---
+++ src/pages/not_in_use/NIU_Home.jsx
@@ -0,0 +1,148 @@
+// import React, { useEffect, useState } from 'react';
+// import { View, Text, Image, TouchableOpacity } from 'react-native';
+// import moment from 'moment';
+// import LinearGradient from 'react-native-linear-gradient';
+// import user from '../../../images/user.png'
+// import check_mark from '../../../images/check_mark.png'
+// import fire_logo from '../../../images/fire_logo.png'
+// const Hello = ({ name, picture }) => {
+//   return (
+//     <View className='flex-row mb-5'>
+//       <View className='flex-col mr-[55px]'>
+//         <Text className='text-white text-[30px] font-bold'>Hello, {name}</Text>
+//         <Text className='text-white text-[15px] font-bold'>lets make your taking pills easy..!</Text>
+//       </View>
+//       <TouchableOpacity>
+//         <View className='bg-white rounded-full w-[60px] items-center'>
+//           <Image source={`${picture}`} className='h-[60px] w-[50px]' />
+//         </View>
+//       </TouchableOpacity>
+//       {/* <View className='bg-white rounded-full w-[60px] items-center'>
+//         <Image source={`${picture}`} className='h-[60px] w-[50px]' />
+//       </View> */}
+//     </View>
+//   )
+// }
+// const Today = ({ value }) => {
+//   return (
+//     <View>
+//       <Text className='text-black text-[17px] font-bold'>Today's Adherence</Text>
+//       <Text className='text-[#7034A4] text-[50px] font-bold'>{value}%</Text>
+//     </View>
+//   )
+// }
+// const Yesterday = ({ value }) => {
+//   return (
+//     <View className='flex-row items-center mb-4'>
+//       <Text className='text-black text-[17px] font-bold'>Yesterday: </Text>
+//       <Text className='text-[#7034A4] text-[20px] font-bold'>{value}%</Text>
+//     </View>
+//   )
+// }
+// const Today_Progress = ({ progress }) => {
+//   return (
+//     <View className="h-[25px] mb-4 bg-[#E2E1F3] w-[100%] rounded-full">
+//       <View className={`h-full ${progress} bg-[#AB33ED] rounded-full`} />
+//     </View>
+//   )
+// }
+// const Today_Medicine = () => {
+//   return (
+//     <View className='flex-row items-center'>
+//       <Text className='text-black text-[20px] font-bold mr-10'>My Medication</Text>
+//       <View className='bg-[#7034A4] h-[30px] w-[30px] rounded-lg justify-center items-center mr-4'>
+//         <Image source={check_mark} />
+//       </View>
+//       <View className='bg-[#7034A4] h-[30px] w-[30px] rounded-lg justify-center items-center mr-4'>
+//         <Text className='text-white text-[20px] font-bold'>A</Text>
+//       </View>
+//       <View className='bg-[#7034A4] h-[30px] w-[30px] rounded-lg justify-center items-center'>
+//         <Text className='text-white text-[20px] font-bold'>E</Text>
+//       </View>
+//     </View>
+//   )
+// }
+// const Streak = ({ value }) => {
+//   return (
+//     <View>
+//       <Text className='text-black text-[17px] font-bold mb-1'>{value} days streak, Keep up!!!</Text>
+//       <Text className='text-[#A531E9] font-bold'>Way to go!</Text>
+//     </View>
+//   )
+// }
+// const Days = ({ color, margin, day, marginText }) => {
+//   return (
+//     <View className={`items-center ${margin}`}>
+//       <View className={`${color} h-[23px] w-[23px] rounded-full`} />
+//       <Text className={`text-[#A0A0A0] ${marginText} font-bold`}>{day}</Text>
+//     </View>
+//   )
+// }
+// const Home = () => {
+// const [weekDates, setWeekDates] = useState([]);
+// const [monthLabel, setMonthLabel] = useState('');
+// useEffect(() => {
+//   const startOfWeek = moment().startOf('isoWeek');
+//   const newWeek = Array.from({ length: 7 }, (_, i) => {
+//     const day = moment(startOfWeek).add(i, 'days');
+//     return {
+//       date: day.format('YYYY-MM-DD'),
+//       label: day.format('ddd'),
+//       number: day.format('D'),
+//     };
+//   });
+//   setWeekDates(newWeek);
+//   setMonthLabel(startOfWeek.format('MMMM'));
+// }, []);
+//   return (
+//     <View className="flex-1 justify-center items-center bg-[#A532E9]">
+// <Hello name={'User'} picture={user} />
+// <View className="bg-white w-[90%] rounded-3xl h-[120px] p-2 items-center justify-center mb-5">
+//   <Text className="text-[18px] font-bold text-[#9838CD] mb-3">{monthLabel}</Text>
+//   <View className="flex-row justify-between w-full">
+//     {weekDates.map((day) => (
+//       <View
+//         key={day.date}
+//         className={`items-center px-2 py-2 rounded-full ${day.date === moment().format('YYYY-MM-DD') ? 'bg-[#AB33ED]' : 'bg-transparent'}`}>
+//         <Text className={`text-lg font-bold ${day.date === moment().format('YYYY-MM-DD') ? 'text-white' : 'text-[#171D1B]'}`}>{day.number}</Text>
+//         <Text className={`text-sm font-bold ${day.date === moment().format('YYYY-MM-DD') ? 'text-white' : 'text-[#A531E9]'}`}>{day.label}</Text>
+//       </View>
+//     ))}
+//   </View>
+// </View>
+// <View className='w-[90%] h-[260px]'>
+//   <LinearGradient
+//     // colors={['#EEAECA', '#FFFFFF', '#EEAECA']}
+//     colors={['#FBC2EB', '#FFFFFF', '#FBC2EB']}
+//     locations={[0.1, 0.5, 0.9]}
+//     start={{ x: 0, y: 1 }}
+//     end={{ x: 1, y: 0 }}
+//     className='h-[100%] w-[100%] items-start justify-center rounded-3xl p-5'>
+//     <Today value={35} />
+//     <Yesterday value={0} />
+//     <Today_Progress progress={'w-[30%]'} />
+//     <View className="h-[2px] w-[100%] bg-pink-400 mb-4 rounded-full" />
+//     <Today_Medicine />
+//   </LinearGradient>
+// </View>
+// <View className="h-[2px] w-[90%] bg-[#DB9DFFDB] rounded-full my-5" />
+//       <View className='w-[90%] h-[140px] bg-white mb-16 rounded-3xl flex-row items-center'>
+//         <Image source={fire_logo} />
+//         <View>
+//           <Streak value={14} />
+//           <View className="h-[1px] w-[100%] bg-[#DB9DFFDB] my-3 rounded-full" />
+//           <View className='flex-row'>
+//             <Days marginText={'mt-1'} margin={'mr-2'} day={'M'} color={'bg-[#FA4F00]'} />
+//             <Days marginText={'mt-1'} margin={'mr-2'} day={'T'} color={'bg-[#FA4F00]'} />
+//             <Days marginText={'mt-1'} margin={'mr-2'} day={'W'} color={'bg-[#FA4F00]'} />
+//             <Days marginText={'mt-1'} margin={'mr-2'} day={'T'} color={'bg-[#FA4F00]'} />
+//             <Days marginText={'mt-1'} margin={'mr-2'} day={'F'} color={'bg-[#CFCFCF]'} />
+//             <Days marginText={'mt-1'} margin={'mr-2'} day={'S'} color={'bg-[#CFCFCF]'} />
+//             <Days marginText={'mt-1'} day={'S'} color={'bg-[#CFCFCF]'} />
+//           </View>
+//         </View>
+//       </View>
+//     </View>
+//   )
+// }
+// export default Home;
--- src/pages/notifications/Notifications.jsx
+++ src/pages/notifications/Notifications.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, ScrollView } from 'react-native';
 const Notification = ({ title, message, title_color }) => {
     return (
-        <View className='px-[3%] py-[3%] m-[5%] bg-[#FFFFFF] w-[90%] shadow-md shadow-black rounded-xl'>
+        <View className='px-[3%] py-[3%] m-[5%] bg-white w-[90%] shadow-md shadow-black rounded-xl'>
             <Text className={'font-bold text-[21px] text-center'} style={{ color: title_color }}>{title}</Text>
             <Text className='text-[#686868] font-bold text-[13px] text-center'>{message}</Text>
         </View>
@@ -47,36 +47,3 @@ const Notifications = () => {
     );
 };
 export default Notifications;
-
-
-
-
-
-// import React from 'react';
-// import { View, Text } from 'react-native';
-// const Notification = ({ title, message, title_color }) => {
-//     return (
-//         <View className='p-1 bg-[#FFFFFF] h-[13%] w-[90%] mb-3 shadow-md shadow-black justify-center items-center rounded-xl'>
-//             <Text className={'font-bold text-[21px]'} style={{color: title_color}}>{title}</Text>
-//             <Text className='text-[#686868] font-bold text-[13px]'>{message}</Text>
-//         </View>
-//     )
-// }
-// const Notifications = () => {
-//     return (
-//         <View className='flex-1 items-center bg-white justify-center'>
-{/* <View className='flex-1 items-center justify-center absolute top-0 bg-[#A531E9] w-[110%] h-[25%] rounded-b-[300px]'>
-    <Text className='text-white text-[35px] font-bold'>Notification</Text>
-</View> */}
-//             <View className='mt-[23%] w-[100%] items-center justify-center'>
-// <Text className='text-[#A531E9] text-[35px] font-bold mb-3'>Today</Text>
-// <Notification title={'ALERT !'} title_color={'#FF0000'} message={'Today morning dose is not taken yet .'} />
-// <Notification title={'Hey User !'} title_color={'#222222'} message={'Today morning dose is taken late at 9:25 AM .'} />
-// <Notification title={'Hey User !'} title_color={'#FF0000'} message={"It's time to take your medicine - your health matters!"} />
-// <Notification title={'Alert !'} title_color={'#FF0000'} message={"Your medicine schedule is empty - let's add your dose"} />
-// <Notification title={'Hey User !'} title_color={'#FF0000'} message={'Skipping doses can lead to complications. Stay disciplined'} />
-//             </View>
-//         </View>
-//     );
-// };
-// export default Notifications;
